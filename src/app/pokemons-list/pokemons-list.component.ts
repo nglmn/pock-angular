60bd535dfe34e2c59af01b8d376063f448a4a9ff
Artem Liskovshchenko <artem140742@gmail.com>
2024-08-29 14:47:37 +0300

    add logic for switch request

--- src/app/pokemons-list/pokemons-list.component.ts
+++ src/app/pokemons-list/pokemons-list.component.ts
@@ -8,6 +8,7 @@ import {
 import { HttpClient } from '@angular/common/http';
 import { PokemonData } from '../pokemon-info/pokemon-info.interface';
 import { PokemonService } from '../services/pokemon-service.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-pokemons-list',
@@ -23,10 +24,34 @@ export class PokemonsListComponent {
     private pokemonService: PokemonService
   ) {}
 
+
+
   onShowPokemonInfo(url: string) {
-    this.pokemonService.getOnePokemonData(url).subscribe((response: any) => {
+    const pokemonID = this.pokemonService.getIdFromUrl(url);
+
+    this.pokemonService.getOnePokemonData(pokemonID).subscribe((response: any) => {
       const pokemonStatistics = this.pokemonService.adaptStat(response.stats);
       this.selectedPokemon = { name: response.name, pokemonStatistics };
     });
+  }
+
+  onShowLocations(url: string) {
+    const pokemonID = this.pokemonService.getIdFromUrl(url);
+
+    this.pokemonService.getPokemonLocation(pokemonID)
+      .pipe(
+        switchMap((response: any): any =>
+        {
+          const locationAreaUrl = response[0]?.location_area.url;
+          return this.pokemonService.getIndexFromLocationArea(locationAreaUrl);
+        }) //switch map switch to another observable, and close the previous observable 
+      )
+      .subscribe({
+        next: (response: any) => {
+          // console.log("name:", response[0]?.location_area.name);
+          console.log("game-index:", response.game_index);
+        },
+        error: (e) => console.log("location area is unknown")
+      })
   }
 }
--- src/app/services/pokemon-service.service.ts
+++ src/app/services/pokemon-service.service.ts
@@ -13,14 +13,31 @@ import {
 export class PokemonService {
   constructor(public http: HttpClient) {}
 
-  private linkApi: string = 'https://pokeapi.co/api/v2/pokemon/';
+  private pokemonDataApi: string = 'https://pokeapi.co/api/v2/pokemon';
+  private pokemonLocationAreaApi: string = 'https://pokeapi.co/api/v2/location-area';
 
   getPokemons() {
-    return this.http.get<PokemonsList>(this.linkApi);
+    return this.http.get<PokemonsList>(this.pokemonDataApi);
   }
 
-  getOnePokemonData(url: string) {
-    return this.http.get(url);
+  getOnePokemonData(id: number) {
+    return this.http.get(`${this.pokemonDataApi}/${id}`);
+  }
+
+  getPokemonLocation(pokemonId: number) {
+    return this.http.get(`${this.pokemonDataApi}/${pokemonId}/encounters`)
+  }
+
+  getIndexFromLocationArea(locationUrl: string) {
+    return this.http.get(locationUrl);
+  }
+
+
+
+  getIdFromUrl(url: string): number {
+    const separatedUrl = url.split("/")
+    const pokemonId = separatedUrl[separatedUrl.length - 2];
+    return +pokemonId;
   }
 
   adaptStat(statistics: InputStatistic[]): Statistics {
